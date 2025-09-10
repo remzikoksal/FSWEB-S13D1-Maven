f156955df38562126da1616e23a5e25c437e3c0f
remzikoksal <remzikksal@hotmail.com>
2025-09-10 18:08:26 +0300

    Proje bitti

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,23 +5,42 @@ public class Main {
         System.out.println("Hello world!");
     }
 
-    public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        
+    public static boolean shouldWakeUp(boolean isBarking, int clock){
+       if (clock < 0 || clock > 23 || !isBarking){
+           return false;
+       }
+       return clock >=20 || clock <8;
     }
 
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       
+        if ((firstAge >= 13 && firstAge <= 19) ||
+                (secondAge >= 13 && secondAge <= 19) ||
+                (thirdAge >= 13 && thirdAge <= 19)) {
+            return true;
+        }
+        return false;
     }
 
     public static boolean isCatPlaying(boolean isSummer, int temp) {
-     
+        int upperLimit = isSummer ? 45 : 35;
+        return (temp >= 25 && temp <= upperLimit);
     }
 
+
     public static double area(double width, double height) {
-       
+
+        if (width < 0 || height < 0) {
+            return -1;
+        }
+        return width * height;
     }
 
     public static double area(double radius) {
-       
+        if (radius < 0) {
+            return -1;
+        }
+        return radius * radius * Math.PI;
     }
+
 }
+
